bbe15b9a5e71c81898de0dd84cd7070caf372f19
John Paul Jaquith <japesjake@gmail.com>
2025-04-24 01:58:11 -0400

    implemented ball movement in the y axis

--- barrels.py
+++ barrels.py
@@ -1,5 +1,8 @@
 import pygame as pg
 import math as m
+import time, os
+# start_time=time.time()
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,100)
 pg.init()
 surface=pg.display.set_mode((800,800))
 pg.display.set_caption("Barrels")
@@ -7,20 +10,27 @@ pg.display.set_caption("Barrels")
 SILVER=(192,192,192)
 class Circle:
     def __init__(self,x,y):
+        self.sx=x
+        self.sy=y
         self.x=x
         self.y=y
         self.v=1
         self.g=1
-        self.theta=135
+        self.t=0
+        self.theta=90
         self.flying=False
         self.blit()
+        pg.display.update()
     def blit(self):
-        pg.draw.circle(surface,SILVER,(self.x,self.y),5)
+        pg.draw.circle(surface,SILVER,(round(self.x),round(self.y)),5)
     def draw(self):
         if True:
-            self.y=(round(self.x)*round(m.tan(self.theta))-self.g*round(self.x)^2)/(2*self.v^2*round(m.cos(self.theta))^2)
-            self.x=(round(self.y)*round(m.tan(self.theta))-self.g*round(self.y)^2)/(2*self.v^2*round(m.cos(self.theta))^2)
+            # self.y=(round(self.x)*round(m.tan(self.theta))-self.g*round(self.x)^2)/(2*self.v^2*round(m.cos(self.theta))^2)
+            # self.x=(round(self.y)*round(m.tan(self.theta))-self.g*round(self.y)^2)/(2*self.v^2*round(m.cos(self.theta))^2)
+            self.t+=1
+            self.y=self.sx+self.v*self.t*m.cos(self.theta)
             circle.blit()
+
 circle=Circle(400,400)
 
 running=True
@@ -31,6 +41,6 @@ while running:
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_SPACE:
                 circle.flying=True
-        circle.draw()
-        pg.display.update()
+    circle.draw()
+    pg.display.update()
 
