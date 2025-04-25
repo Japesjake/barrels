3e9e14599aac5f500f31fb8814d9d22e8889a635
John Paul Jaquith <japesjake@gmail.com>
2025-04-25 06:43:49 -0400

    added reset to flight after t=50 and added click to activate.

--- barrels.py
+++ barrels.py
@@ -4,12 +4,15 @@ import time, os
 start_time=time.time()
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,100)
 pg.init()
-surface=pg.display.set_mode((800,800))
+WIDTH=800
+HEIGHT=800
+surface=pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption("Barrels")
 
 SILVER=(192,192,192)
 class Circle:
-    def __init__(self,x,y):
+    def __init__(self,x,y,ret):
+        self.ret=ret
         self.sx=x
         self.sy=y
         self.x=x
@@ -19,17 +22,16 @@ class Circle:
         self.g=-1
         self.t=0
         self.theta=45
-        self.flying=False
+        self.launch=False
         pg.draw.circle(surface,SILVER,(self.x,self.y),5)
     def draw(self):
-        if True:
+        if circle.launch==True:
+            # self.vy=self.sy+self.ret.x
             self.t+=0.1
             self.x=self.sx+self.vx*self.t
             self.y=self.sy+self.vy*self.t+(0.5)*self.g*self.t**2
-            self.y=800-self.y
-            # make sure to add starting location to left of self.x on the left side.
-            # self.y=self.x*m.tan(self.theta)-self.g*self.x**2/2*(self.v^2)*(m.cos(self.theta)**2)
-            pg.draw.circle(surface,SILVER,(self.x,self.y),5)
+            self.y=HEIGHT-self.y
+        pg.draw.circle(surface,SILVER,(self.x,self.y),5)
 class Reticle:
     def __init__(self,x,y):
         self.x=x
@@ -38,20 +40,22 @@ class Reticle:
         self.x,self.y=pg.mouse.get_pos()
         pg.draw.line(surface,(255,0,0),(self.x,self.y-10),(self.x,self.y+10))
         pg.draw.line(surface,(255,0,0),(self.x-10,self.y),(self.x+10,self.y))
-circle=Circle(400,400)
 reticle=Reticle(-1,-1)
+circle=Circle(400,400,reticle)
 
 running=True
 while running:
     for event in pg.event.get():
         if event.type==pg.QUIT:
             running=False
-        if event.type==pg.KEYDOWN:
-            if event.key==pg.K_SPACE:
-                circle.flying=True
-    # pg.time.delay(5)
-    # surface.fill((0,0,0))
+        if event.type==pg.MOUSEBUTTONDOWN:
+            circle.launch=True
+    if circle.launch==True and circle.t>=50:
+        circle.x=circle.sx
+        circle.y=circle.sy
+        circle.t=0
+        circle.launch=False
+    surface.fill((0,0,0))
     circle.draw()
     # reticle.draw()
     pg.display.update()
-    # print(pg.mouse.get_pos())
